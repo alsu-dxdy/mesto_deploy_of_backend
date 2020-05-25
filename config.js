dc04cd098c0e40d8256df19de8b9ef71136a7346
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-05-25 21:32:42 +0500

    deleted parseInt for JWT_secret in config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   DATABASE_URL: 'mongodb://localhost:27017/mestodb',
 
   // eslint-disable-next-line radix
-  JWT_SECRET: parseInt(process.env.JWT_SECRET) || 'JWT_SECRET',
+  JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
 
 };
 
