47449800739b656cc937b4c540b20972ad739a5c
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-05-25 23:10:55 +0500

    added avatarSchema

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,13 @@ const { celebrate } = require('celebrate');
 const { getUsers, getUserById, removeUserdById, updateUser, updateAvatarUser } = require('../controllers/users');
 const { userIdSchema } = require('../schemas/userId');
 const { nameAboutUser } = require('../schemas/nameAboutUser');
+const { avatarSchema } = require('../schemas/avatar');
 
 
 router.get('/', getUsers);
 router.get('/:userId', celebrate(userIdSchema), getUserById);
 router.delete('/:userId', celebrate(userIdSchema), removeUserdById);
 router.patch('/me', celebrate(nameAboutUser), updateUser);
-router.patch('/me/avatar', updateAvatarUser);
+router.patch('/me/avatar', celebrate(avatarSchema), updateAvatarUser);
 
 module.exports = router;
