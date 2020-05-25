e930f38769bfa1b4eef60f0a62c55c0a9dfea89d
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-05-25 23:07:24 +0500

    signUpSchema>avatar>regex, added nameAboutUser

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ const { celebrate } = require('celebrate');
 // eslint-disable-next-line object-curly-newline
 const { getUsers, getUserById, removeUserdById, updateUser, updateAvatarUser } = require('../controllers/users');
 const { userIdSchema } = require('../schemas/userId');
+const { nameAboutUser } = require('../schemas/nameAboutUser');
 
 
 router.get('/', getUsers);
 router.get('/:userId', celebrate(userIdSchema), getUserById);
 router.delete('/:userId', celebrate(userIdSchema), removeUserdById);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate(nameAboutUser), updateUser);
 router.patch('/me/avatar', updateAvatarUser);
 
 module.exports = router;
--- schemas/signup.js
+++ schemas/signup.js
@@ -4,7 +4,9 @@ module.exports.signUpSchema = {
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().url().required(),
+    avatar: Joi.string()
+      .required()
+      .regex(/^http(s)?:\/\/(www\.)?(?!(www\.))(([0-2][0-5][0-5]\.){3}[0-2][0-5][0-5]|([a-z0-9]{2,}[-a-z0-9]*\.)+[a-z]{2,6})(\/)?(:\d{2,5})?([-?=&A-Za-z0-9.]{2,}\/?)*#?\d?$/),
     email: Joi.string().email().required(),
     password: Joi.string()
       .min(8)
